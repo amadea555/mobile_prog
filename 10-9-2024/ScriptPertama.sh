6a26a6c466aa1913af2cf33777420ac91cdc0cd1
amadea555 <kenzoamadea@gmail.com>
2024-09-12 10:32:37 +0700

    ini file 10-9-2024

---
+++ 10-9-2024/ScriptPertama.sh
@@ -0,0 +1,51 @@
+#echo "Siapa nama anda?"
+
+#command ini untuk berinteraksi dengan terminal
+#setelah read merupakan nama variabel yang digunakan untuk menampung jawaban atau interaksi dari user atau pengguna terhadap terminal
+#read name
+#untuk memanggil variabel yang kita gunakan untuk menampung jawaban atau interaksi gunakan $
+#echo "nama saya adalah $name"
+
+#input panjang persegi panjang
+#echo "masukan panjang: "
+#read panjang
+
+#input lebar persegi panjang
+#echo "masukan lebar: "
+#read lebar
+
+#menghitung keliling persegi panjang
+#keliling=$((2 * (panjang + lebar))
+
+#menampilkan hasil perhitungan
+#echo "Keliling persegi panjang dengan panjang $panjang dan lebar $lebar adalah: $keliling"
+
+#!/bin/bash
+
+# Meminta input jari-jari dan tinggi
+#echo "Masukkan jari-jari kerucut: "
+#read jari
+
+#echo "Masukkan tinggi kerucut: "
+#read tinggi
+
+# Menghitung volume kerucut menggunakan bc
+#volume=$((1/3) * (3.14) * $jari * $jari * $tinggi))
+
+# Menampilkan hasil
+#echo "Volume kerucut adalah: $volume"
+
+#untuk membuat sebuah function di scriptbash cukup hanya memberikan nama fungsi saja setelah function tanpa menggunakan parameter
+#function nm_fungsi(){
+    #isi function
+# }
+
+#untuk pemanggilan functionn cukup panggil nama functionnya saja
+#nm_fungsi
+
+#function hello(){
+    #echo "Selamat Datang"
+#}
+
+#hello
+
